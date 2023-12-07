5387b693f9d6334a912dd2ded854d328fe01bef4
JamesBoiVincent <53995435+JamesBoiVincent@users.noreply.github.com>
2023-12-07 17:17:03 +0800

    Add files via upload

---
+++ src/java/context/listener/HakiFitAppListener.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package context.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class HakiFitAppListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // Perform actions on context initialization
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Perform actions on context destruction
+    }
+}
