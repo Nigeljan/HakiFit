38e88086e84c722c4e38a7fa9966674d30158842
JamesBoiVincent <53995435+JamesBoiVincent@users.noreply.github.com>
2023-12-09 00:43:49 +0800

    Update HakiFitCategorySelection.java

--- src/java/servlet/process/HakiFitCategorySelection.java
+++ src/java/servlet/process/HakiFitCategorySelection.java
@@ -6,6 +6,7 @@ package servlet.process;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,29 +18,39 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class HakiFitCategorySelection extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+    String cardioInit;
+    String strengthTrainingInit;
+    String yogaInit;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        cardioInit = config.getInitParameter("cardio");
+        strengthTrainingInit = config.getInitParameter("strengthTraining");
+        yogaInit = config.getInitParameter("yoga");
+    }
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet HakiFitCategorySelection</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet HakiFitCategorySelection at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            
+            String buttonValue = request.getParameter("button");
+            
+            if(cardioInit.equals(buttonValue)){
+                request.setAttribute("buttonValue_fromp1", buttonValue);
+                request.getRequestDispatcher("HakiFit_Category_Cardio.jsp").forward(request, response);
+            }  
+            
+            if(strengthTrainingInit.equals(buttonValue)){
+                request.setAttribute("buttonValue_fromp1", buttonValue);
+                request.getRequestDispatcher("HakiFit_Category_StrengthTraining.jsp").forward(request, response);
+            }  
+            
+            if(yogaInit.equals(buttonValue)){
+                request.setAttribute("buttonValue_fromp1", buttonValue);
+                request.getRequestDispatcher("HakiFit_Category_Yoga.jsp").forward(request, response);
+            }  
         }
     }
 
