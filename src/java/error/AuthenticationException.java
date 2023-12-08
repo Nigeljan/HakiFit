5197ff95a7a314c26bd56f3acd1c7f09d08b414c
JamesBoiVincent <53995435+JamesBoiVincent@users.noreply.github.com>
2023-12-08 18:21:57 +0800

    Add files via upload

---
+++ src/java/error/AuthenticationException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package error;
+
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
---
+++ src/java/error/NullValueException.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package error;
+
+public class NullValueException extends Exception {
+    public NullValueException(String message) {
+        super(message);
+    }
+}
